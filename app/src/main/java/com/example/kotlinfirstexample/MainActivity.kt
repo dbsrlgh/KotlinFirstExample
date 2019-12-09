f6779a7a72a9e50bdfc9940f9582bda1325b5668
dbsrlgh <kihoyoun164938@gmail.com>
2019-12-09 15:18:10 +0900

    안드로이드 with Kotlin 프로그래밍 가이드 로또 번호 생성기 예제앱

--- app/src/main/java/com/example/kotlinfirstexample/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfirstexample/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.kotlinfirstexample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_absolute.*
 
 // 수정 및 업데이트 테스트 시작
 
@@ -10,5 +12,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // 랜덤으로 번호 생성 카드의 클릭 이벤트 리스너
+        randomCard.setOnClickListener {
+            // ResultActivity 를 시작하는 Intent 생성
+            val intent = Intent(this, ResultActivity::class.java)
+            // intent 의 결과 데이터를 전달한다.
+            // int 의 리스트를 전달하므로 putIntegerArrayListExtra 를 사용한다.
+            intent.putIntegerArrayListExtra("result", ArrayList(LottoNumberMaker.getShuffleLottoNumbers()))
+            // ResultActivity 를 시작하는 Intent 를 만들고 startActivity 로 실행
+            startActivity(intent)
+        }
+        // 별자리로 번호 생성 카드의 클릭 이벤트 리스너
+        constellationCard.setOnClickListener {
+            // ConstellationActivity 를 시작하는 Intent 를 만들고 startActivity 로 실행
+            startActivity(Intent(this, ConstellationActivity::class.java))
+        }
+        // 이름으로 번호 생성 카드의 클릭 이벤트 리스너
+        nameCard.setOnClickListener {
+            // NameActivity 를 시작하는 Intent 를 만들고 startActivity 로 실행
+            startActivity(Intent(this, NameActivity::class.java))
+        }
     }
 }
