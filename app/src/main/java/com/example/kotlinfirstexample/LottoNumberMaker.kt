1483f383ded93e57d4abee020267b0f9591c0b02
dbsrlgh <kihoyoun164938@gmail.com>
2019-12-09 14:47:40 +0900

    안드로이드 with Kotlin 앱 프로그래밍 가이드 로또 번호 생성기 예제

---
+++ app/src/main/java/com/example/kotlinfirstexample/LottoNumberMaker.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinfirstexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LottoNumberMaker : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lotto_number_maker)
+    }
+}
